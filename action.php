2c00dbd03a8661a26240d78106746cbffb162b9a
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2021-01-11 11:40:56 +0900

    NEW: ROMANCE DAWN - 2021

---
+++ action.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * op-module-webpack:/css/common/action.php
+ *
+ * @created   2018-04-12
+ * @version   1.0
+ * @package   op-module-webpack
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+return [
+	__DIR__.'/reset',
+	__DIR__.'/root',
+	__DIR__.'/body',
+	__DIR__.'/border',
+	__DIR__.'/font',
+	__DIR__.'/font-size',
+	__DIR__.'/font-style',
+	__DIR__.'/font-color',
+	__DIR__.'/br',
+	__DIR__.'/hr',
+	__DIR__.'/a',
+	__DIR__.'/h',
+	__DIR__.'/p',
+	__DIR__.'/list',
+	__DIR__.'/code',
+	__DIR__.'/pre',
+	__DIR__.'/form',
+	__DIR__.'/form-button',
+	__DIR__.'/table',
+	__DIR__.'/cursor',
+	__DIR__.'/wai-aria',
+	__DIR__.'/appearance',
+	__DIR__.'/other',
+	__DIR__.'/breadcrumbs',
+	__DIR__.'/pager',
+	__DIR__.'/tips',
+	__DIR__.'/markdown',
+	__DIR__.'/debug',
+];
