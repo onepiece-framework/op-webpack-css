5ed8aed8135b301c975fd970714dc6930ca4ee25
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-11-19 23:59:31 +0900

    CI: ci.sh

---
+++ ci.sh
@@ -0,0 +1,12 @@
+
+ ## op-unit-app:/ci.sh
+ #
+ # Call from git pre-push
+ #
+ # @created   2022-11-14
+ # @version   1.0
+ # @package   op-unit-app
+ # @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ # @copyright Tomoaki Nagahara All right reserved.
+
+php ../../../ci.php display=0
