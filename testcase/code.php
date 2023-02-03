2419d7d1cbc8d94c6f6f76df4b2e4efc19111b94
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-03 16:45:41 +0900

    New: testcase > code

---
+++ testcase/code.php
@@ -0,0 +1,11 @@
+<?php
+/** op-webpack-css:/testcase/code.php
+ *
+ * @created   2023-02-03
+ * @version   1.0
+ * @package   op-webpack-css
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+?>
+<code>$var = 1 + 1;</code>
