500fa983d8552644991e65a7cc2a18017c7e5442
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-02-03 16:46:07 +0900

    New: testcase > markdown

---
+++ testcase/markdown.php
@@ -0,0 +1,10 @@
+<?php
+/** op-webpack-css:/testcase/markdown.php
+ *
+ * @created   2023-02-03
+ * @version   1.0
+ * @package   op-webpack-css
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+?>
