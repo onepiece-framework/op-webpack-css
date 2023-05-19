02475a5bd25910a624189fdb7490dd073d8c7780
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-05-19 21:02:41 +0900

    New: testcase > margin.php

---
+++ testcase/margin.php
@@ -0,0 +1,22 @@
+<?php
+/** op-webpack-css:/testcase/margin.php
+ *
+ * @created   2023-05-09
+ * @version   1.0
+ * @package   op-webpack-css
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+?>
+<style>
+#margin div {
+    border: 1px solid black;
+}
+</style>
+<div id="margin">
+    <div class="margin">margin</div>
+    <div class="margin.top">margin.top</div>
+    <div class="margin.right">margin.right</div>
+    <div class="margin.left">margin.left</div>
+    <div class="margin.bottom">margin.bottom</div>
+</div>
